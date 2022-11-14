b11188371e44c1b5f297d05cbbf049284c9699c2
Biktor <biktor@biktorgj.com>
2022-11-14 09:32:46 +0100

    Remove references to the speaker in callaudiocli

--- tools/callaudiocli.c
+++ tools/callaudiocli.c
@@ -15,13 +15,11 @@ int main (int argc, char *argv[0])
     g_autoptr(GOptionContext) opt_context = NULL;
     g_autoptr(GError) err = NULL;
     int mode = -1;
-    int speaker = -1;
     int mic = -1;
     gboolean status = FALSE;
 
     const GOptionEntry options [] = {
         {"select-mode", 'm', 0, G_OPTION_ARG_INT, &mode, "Select mode", NULL},
-        {"enable-speaker", 's', 0, G_OPTION_ARG_INT, &speaker, "Enable speaker", NULL},
         {"mute-mic", 'u', 0, G_OPTION_ARG_INT, &mic, "Mute microphone", NULL},
         {"status", 'S', 0, G_OPTION_ARG_NONE, &status, "Print status", NULL},
         { NULL, 0, 0, G_OPTION_ARG_NONE, NULL, NULL, NULL }
@@ -40,17 +38,11 @@ int main (int argc, char *argv[0])
     }
 
     /* If there's nothing else to be done, print the current status */
-    if (mode == -1 && speaker == -1 && mic == -1)
+    if (mode == -1 && mic == -1)
         status = TRUE;
 
     if (mode == CALL_AUDIO_MODE_DEFAULT || mode == CALL_AUDIO_MODE_CALL) {
         if (!call_audio_select_mode(mode, &err)) {
-            return 1;
-        }
-    }
-
-    if (speaker == 0 || speaker == 1) {
-        if (!call_audio_enable_speaker((gboolean)speaker, &err)) {
             return 1;
         }
     }
@@ -63,16 +55,25 @@ int main (int argc, char *argv[0])
 
     if (status) {
         CallAudioMode audio_mode = call_audio_get_audio_mode();
-        CallAudioSpeakerState speaker_state = call_audio_get_speaker_state();
         CallAudioMicState mic_state = call_audio_get_mic_state();
         const char *string_audio = g_enum_to_string(CALL_TYPE_AUDIO_MODE, audio_mode);
-        const char *string_speaker = g_enum_to_string(CALL_TYPE_AUDIO_SPEAKER_STATE, speaker_state);
         const char *string_mic = g_enum_to_string(CALL_TYPE_AUDIO_MIC_STATE, mic_state);
-
+        GVariant *available_outputs = call_audio_get_available_devices();
+        guint dev_id;
+        guint dev_verb;
+        gchar *card_name;
+        GVariantIter *iter;
         g_print("Selected mode: %s\n"
-                "Speaker enabled: %s\n"
                 "Mic muted: %s\n",
-                string_audio, string_speaker, string_mic);
+                string_audio, string_mic);
+        g_print("Available outputs:\n");
+        g_variant_get (available_outputs, "a(uus)", &iter);
+        while (g_variant_iter_loop (iter, "(uus)", &dev_id, &dev_verb, &card_name))
+        {
+        g_print ("%i %s (verb %i)", dev_id, card_name, dev_verb);
+        }
+    g_variant_iter_free (iter);
+    g_print("END\n");
     }
 
     call_audio_deinit ();
